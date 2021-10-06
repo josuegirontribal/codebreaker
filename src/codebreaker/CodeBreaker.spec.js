18d7824583f91835ae7b33977714965ef6a1f429
Josue Giron <jgiron@tribalworldwide.gt>
2021-10-06 12:12:26 -0600

    Primera implementación de numero random con un solo digito

--- src/codebreaker/CodeBreaker.spec.js
+++ src/codebreaker/CodeBreaker.spec.js
@@ -4,12 +4,14 @@ describe("CodeBreaker", () => {
   let numeroInput;
   let boton;
   let resultado;
+  let randNumber;
 
   beforeAll(() => {
     document.body.innerHTML = fs.readFileSync("index.html", "utf8");
     require("../codebreaker/presenter");
 
     numeroInput = document.querySelector("#numero");
+    randNumber = document.querySelector("#rand-number");
     boton = document.querySelector("#boton");
     resultado = document.querySelector("#div-resultado");
   });
@@ -26,12 +28,25 @@ describe("CodeBreaker", () => {
 
   it("Ingreso un numero que es correcto", () => {
     ingreso(8);
+    setRandNum(8)
+    intentoAdivinar();
+    expect(mensajeResultante()).toEqual("Ganaste!");
+  });
+
+  it("Ingreso un numero que es correcto aleatorio", () => {
+    let randNum = getRandNum(0, 9)
+    ingreso(randNum);
+    setRandNum(randNum)
     intentoAdivinar();
     expect(mensajeResultante()).toEqual("Ganaste!");
   });
 
   function ingreso(num) {
     numeroInput.value = num;
+  }
+
+  function setRandNum(num) {
+    randNumber.value = num;
   }
 
   function intentoAdivinar() {
@@ -45,4 +60,8 @@ describe("CodeBreaker", () => {
   afterEach(() => {
     resultado.innerHTML = "";
   });
+
+  function getRandNum(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
 });
--- src/codebreaker/presenter.js
+++ src/codebreaker/presenter.js
@@ -1,14 +1,27 @@
 const boton = document.querySelector("#boton");
 
 boton.addEventListener("click", () => {
+
   const resultado = document.querySelector("#div-resultado");
+  const randNum = document.querySelector("#rand-number");
   const userNum = document.querySelector("#numero");
 
-  let userNumInt = parseInt(userNum.value)
+  let randNumInt = parseInt(randNum.value)
 
-  if ( userNumInt === 8) {
+  if (randNum.value === ""){
+    randNumInt = getRandNum(0, 9)
+  }
+
+  let userNumInt = parseInt(userNum.value)
+  
+
+  if ( userNumInt === randNumInt) {
     resultado.innerHTML = "Ganaste!";
     return
   }
   resultado.innerHTML = "No es el numero correcto";
 });
+
+function getRandNum(min, max) { 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
